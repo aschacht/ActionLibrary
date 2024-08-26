865f2706c06d535bc5d09342b9da0e0cbe2b4295
Wesley <capin_g@hotmail.com>
2024-08-25 20:00:12 -0400

    sdfgdfgdfg

---
+++ ActionLibrary/src/Actions/MoveBetween.java
@@ -0,0 +1,78 @@
+package Actions;
+
+import FlatLander.FlatLander;
+
+public class MoveBetween extends Actions {
+
+	private int moveByx;
+	private int moveByy;
+	private int xLow;
+	private int xHigh;
+	private int yLow;
+	private int yHigh;
+	private ActionStatus actionStatus;
+	boolean xdirect= false;
+	boolean ydirect = false;
+	private Physics physics;
+
+
+
+	public void act() {
+		actionStatus = ActionStatus.STARTED;
+		if(actor.getX()<=xLow) {
+			xdirect = true;
+		}
+		if(actor.getX()>=xHigh) {
+			xdirect = false;
+		}
+		
+		if(actor.getY()<=yLow) {
+			ydirect=true;
+		}
+		
+		if(actor.getY()>=yHigh) {
+			ydirect = false;
+		}
+		if(xdirect) {
+			actor.changeMoveXBy(moveByx);
+			
+		}else {
+			actor.changeMoveXBy(-moveByx);
+		}
+		if(ydirect) {
+			actor.changeMoveYBy(moveByy);
+		}else {
+			actor.changeMoveYBy(-moveByy);
+		}
+		//physics.applyPhysics();
+		actionStatus = ActionStatus.COMPLETE;
+		
+		
+		
+		
+	}
+
+	
+	
+	public MoveBetween(FlatLander actor,int moveByx,int moveByy,int xLow,int xHigh, int yLow, int yHigh, Physics physics) {
+		super(actor);
+		this.moveByx = moveByx;
+		this.moveByy = moveByy;
+		this.xLow = xLow;
+		this.xHigh = xHigh;
+		this.yLow = yLow;
+		this.yHigh = yHigh;
+		this.physics = physics;
+		this.actionStatus = ActionStatus.UNSTARTED;
+		
+	}
+	
+	
+	
+	
+	public ActionStatus getActionStatus() {
+
+		return actionStatus;
+	}
+
+}
